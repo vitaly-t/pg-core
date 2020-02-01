8c0afcde20d6a916d920077058b64ffde2ac4804
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2020-02-01 14:01:29 +0000

    initial check-in

--- test/native/missing-native.js
+++
@@ -1,6 +0,0 @@
-//this test assumes it has been run from the Makefile
-//and that node_modules/pg-native has been deleted
-
-var assert = require('assert');
-
-assert.equal(require('../../lib').native, null);
